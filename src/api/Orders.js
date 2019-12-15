64a473d80475d073855c4a364d9ff7863bb4e207
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-12-15 19:58:29 +0100

    create axios powered functions

--- src/api/Orders.js
+++ src/api/Orders.js
@@ -1,5 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
+import baseUrl from "./baseUrl";
 
-class Orders {
-  
-}
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("dmx_logistics_token");
+
+  return axios.create({
+    headers: {
+      "auth-token": token
+    }
+  });
+};
+
+export const getOrders = (success, failure, activePage) => {
+  axiosWithAuth()
+    .get(`${baseUrl}/api/v1/orders?page=${activePage}`)
+    .then(response => success(response))
+    .catch(error => failure(error));
+};
+
+export const getUser = (success, failure, userId) => {
+  axiosWithAuth()
+    .get(`${baseUrl}/api/v1/users/${userId}`)
+    .then(response => success(response))
+    .catch(error => failure(error));
+};
+
+export const getRiders = (success, failure, userId) => {
+  axiosWithAuth()
+    .get(`${baseUrl}/api/v1/users/${userId}/riders`)
+    .then(response => success(response))
+    .catch(error => failure(error));
+};
