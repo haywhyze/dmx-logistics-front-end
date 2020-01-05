1192ccdeb9845a06d76dab92aa526b80568b02be
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-01-05 18:48:04 +0100

    convert App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NotFound from "./components/404";
 import Main from "./components/Main";
@@ -7,24 +7,22 @@ import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    let routes = (
-      <>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={SignUp} />
-          <Route path="/404" component={NotFound} />
-          <Main />
-        </Switch>
-      </>
-    );
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div>{routes}</div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  let routes = (
+    <>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={SignUp} />
+        <Route path="/404" component={NotFound} />
+        <Main />
+      </Switch>
+    </>
+  );
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div>{routes}</div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
