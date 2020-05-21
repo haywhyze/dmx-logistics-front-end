4cce7cd75be62a7ae1a19485eb8d86e96a542893
haywhyze <haywhyze@gmail.com>
2020-05-21 17:08:19 +0100

    update package.json

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,2 +1,2 @@
-export default 'http://localhost:4900'
-// export default 'https://dmx-backend.herokuapp.com'
+// export default 'http://localhost:4900'
+export default 'https://dmx-backend.herokuapp.com'
